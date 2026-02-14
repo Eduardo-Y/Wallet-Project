59c39a8d0aef113a20d060d140abd05da6ae7a6c
Eduardo-Y <eduardodemelolive@outlook.com>
2026-02-14 09:35:59 -0300

    integrando gráficos padrão da biblioteca à página

--- src/main.js
+++ src/main.js
@@ -6,11 +6,59 @@ document.addEventListener("DOMContentLoaded", () => {
     const $rightButton = document.querySelector("#right-button");
     const $resultSection = document.querySelector("#results-section");
 
+    const $calculateButton = document.querySelector("#calculate-button");
+    const $doughnutChart = document.querySelector("#doughnut-chart");
+    const $lineChart = document.querySelector("#line-chart");
+
     $leftButton.addEventListener("click", (e) => {
         $resultSection.style.transform = "translateX(0)";
     });
 
     $rightButton.addEventListener("click", (e) => {
         $resultSection.style.transform = "translateX(-50%)";
+    });
+
+    $calculateButton.addEventListener("click", (e) => {
+        new Chart($doughnutChart, {
+            type: "doughnut",
+            data: {
+                labels: ["Red", "Blue", "Yellow"],
+                datasets: [
+                    {
+                        label: "Resultado final",
+                        data: [300, 50, 100],
+                        backgroundColor: [
+                            "rgb(255, 99, 132)",
+                            "rgb(54, 162, 235)",
+                            "rgb(255, 205, 86)",
+                        ],
+                        hoverOffset: 4,
+                    },
+                ],
+            },
+        });
+
+        new Chart($lineChart, {
+            type: "line",
+            data: {
+                labels: [
+                    "January",
+                    "February",
+                    "March",
+                    "April",
+                    "May",
+                    "June",
+                ],
+                datasets: [
+                    {
+                        label: "My First Dataset",
+                        data: [65, 59, 80, 81, 56, 55, 40],
+                        fill: true,
+                        borderColor: "rgb(75, 192, 192)",
+                        tension: 0.4,
+                    },
+                ],
+            },
+        });
     });
 });
