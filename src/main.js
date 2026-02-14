4d0766faac004bb0294980b58877c1701b156c6c
Eduardo-Y <eduardodemelolive@outlook.com>
2026-02-14 08:33:26 -0300

    criando repositório com estrutura e página formadas

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import { Chart } from "chart.js/auto";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const $leftButton = document.querySelector("#left-button");
+    const $rightButton = document.querySelector("#right-button");
+    const $resultSection = document.querySelector("#results-section");
+
+    $leftButton.addEventListener("click", (e) => {
+        $resultSection.style.transform = "translateX(0)";
+    });
+
+    $rightButton.addEventListener("click", (e) => {
+        $resultSection.style.transform = "translateX(-50%)";
+    });
+});
